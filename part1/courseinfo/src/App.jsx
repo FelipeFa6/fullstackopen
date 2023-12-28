bdea856cd6285da1d73efcd2954f0d19574f367c
FelipeFa6 <felipe.farias.e1@gmail.com>
2023-12-28 11:38:29 -0300

    Finnished 1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,23 +1,33 @@
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+
+
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
 
     return (
         <div>
             <Header course={course} />
 
             <Content
-                part1={part1} exercise1={exercises1}
-                part2={part2} exercise2={exercises2}
-                part3={part3} exercise3={exercises3}
+                part1={part1}
+                part2={part2}
+                part3={part3}
             />
 
-            <Total total={exercises1 + exercises2 + exercises3}/>
+            <Total total={part1.exercises +part2.exercises + part3.exercises}/>
         </div>
     )
 }
@@ -33,16 +43,16 @@ const Header = (params) => {
 
 const Part = (params) => {
     return (
-        <p>{params.part} {params.exercise}</p>
+        <p>{params.part.name} {params.part.exercises}</p>
     )
 }
 
 const Content = (params) => {
     return (
         <div>
-            <Part part={params.part1} exercise={params.exercise1}/>
-            <Part part={params.part2} exercise={params.exercise2}/>
-            <Part part={params.part3} exercise={params.exercise3}/>
+            <Part part={params.part1}/>
+            <Part part={params.part2}/>
+            <Part part={params.part3}/>
         </div>
     )
 }
