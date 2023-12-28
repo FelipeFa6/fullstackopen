df8fa5d588940c3105d2104c8d5adb66ae3b66b0
FelipeFa6 <felipe.farias.e1@gmail.com>
2023-12-28 12:57:37 -0300

    Added some other example code

--- part1/example/src/App.jsx
+++ part1/example/src/App.jsx
@@ -1,14 +1,13 @@
-const Hello = (props) => {
-    const bornYear = () => {
-        const yearNow = new Date().getFullYear()
-        return yearNow - props.age
-    }
+import { useState } from 'react';
 
+const Hello = ({ name, age }) => {
+    const bornYear = () => new Date().getFullYear() - age
+    
 
     return (
         <div>
             <p>
-                Hello {props.name}, you are {props.age} years old
+                Hello {name}, you are {age} years old
             </p>
             <p>So you were probably born in {bornYear()}</p>
 
@@ -16,7 +15,28 @@ const Hello = (props) => {
     )
 }
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
 const App = () => {
+
+    const [ counter, setCounter ] = useState(0);
+    const increaseByOne = () => setCounter(counter + 1);
+    const decreaseByOne = () => setCounter(counter - 1);
+    const setToZero = () => setCounter(0);
+
+
     const name = 'Peter'
     const age = 10
 
@@ -25,6 +45,20 @@ const App = () => {
             <h1>Greetings</h1>
             <Hello name="Maya" age={26 + 10} />
             <Hello name={name} age={age} />
+            <Display counter={counter}/>
+            <Button
+              onClick={decreaseByOne}
+              text='minus'
+            />           
+            <Button
+              onClick={setToZero}
+              text='zero'
+            />     
+            <Button
+              onClick={increaseByOne}
+              text='plus'
+            />
+
         </div>
     )
 }
