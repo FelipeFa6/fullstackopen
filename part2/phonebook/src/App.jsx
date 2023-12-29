b66a25c446913bce284a6923c5d0eb06508b3919
FelipeFa6 <felipe.farias.e1@gmail.com>
2023-12-29 19:04:59 -0300

    Finished 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+import axios from 'axios';
 
 const App = () => {
 
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', phone: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
-    ])
+    const [persons, setPersons] = useState([])
 
     const [newName, setNewName] = useState('')
     const [newPhone, setNewPhone] = useState('')
@@ -47,6 +44,18 @@ const App = () => {
     const filteredPersons = persons.filter(person =>
         person.name.toLowerCase().includes(filter.toLowerCase())
     );
+
+    const fetchPersons = () => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log(response.data)
+                setPersons(response.data)
+            })
+    }
+
+    useEffect(fetchPersons, [])
 
     return (
         <div>
