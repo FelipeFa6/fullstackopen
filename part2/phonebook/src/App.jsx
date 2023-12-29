a16857a05d0b8a087a19ae7d5fceece8f7fef6bd
FelipeFa6 <felipe.farias.e1@gmail.com>
2023-12-29 18:03:49 -0300

    Finished 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,13 +3,20 @@ import { useState } from 'react'
 const App = () => {
 
     const [persons, setPersons] = useState([
-        {
-            name: 'Arto ellas',
-            phone: '+569' }
-    ]) 
+        { name: 'Arto Hellas', phone: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
+    ])
 
     const [newName, setNewName] = useState('')
     const [newPhone, setNewPhone] = useState('')
+
+    const [filter, setFilter] = useState('')
+
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value);
+    }
 
     const handleNameChange = (event) => {
         setNewName(event.target.value);
@@ -23,7 +30,7 @@ const App = () => {
         event.preventDefault()
 
         if (persons.some(person => person.name === newName && person.phone === newPhone)) {
-            alert(`${newName} already in phonebook`);
+            alert(`${newName} ${newPhone} already registered`);
             return;
         }
 
@@ -33,18 +40,30 @@ const App = () => {
         }
 
         setPersons(copy => [...copy, newPerson])
+        setNewName('');
+        setNewPhone('');
     }
+
+    const filteredPersons = persons.filter(person =>
+        person.name.toLowerCase().includes(filter.toLowerCase())
+    );
 
     return (
         <div>
             <h2>Phonebook</h2>
+
+                <div>
+                    filter: <input onChange={handleFilterChange} value={filter} />
+                </div>
+
+            <h2>add new</h2>
             <form onSubmit={addPerson}>
                 <div>
                     name: <input onChange={handleNameChange} value={newName} />
                 </div>
 
                 <div>
-                    number: <input onChange={handlePhoneChange} value={newPhone} />
+                    phone: <input onChange={handlePhoneChange} value={newPhone} />
                 </div>
 
                 <div>
@@ -54,7 +73,7 @@ const App = () => {
             
             <div>debug: {newName} {newPhone}</div>
             <h2>Numbers</h2>
-            { persons.map((person, index) => (
+            { filteredPersons.map((person, index) => (
                 <div key={index}>
                     - {person.name} {person.phone}
                 </div>
