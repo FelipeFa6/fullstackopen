0c8713aa7f4c2de65b282c2c5d2f0140740d64c7
FelipeFa6 <felipe.farias.e1@gmail.com>
2024-01-12 00:32:52 -0300

    Finished  4.9

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -4,27 +4,31 @@ const app = require('../app')
 
 const api = supertest(app)
 
-describe('GET -> /api/blogs', () => {
+describe('API endpoint test /api/blogs', () => {
     test('blogs are returned in JSON format', async () => {
         await api
             .get('/api/blogs')
             .expect(200)
             .expect('Content-Type', /application\/json/)
     });
-})
 
-//Single id test
+    test('__id toJSON parse', async () => {
+        const response = await api.get('/api/blogs');
+        expect(response.body[0].id).toBeDefined();
+    })
+
 /*
-* describe('GET -> /api/blogs/:id', () => {
-*     test('single blog retreived', async () => {
-*         await api
-*             .get('/api/blogs/65a04ef859788ee90e463095')
-*             .expect(200)
-*             .expect('Content-Type', /application\/json/)
-*     })
-* });
-*/
+ * //Single id test
+ * test('single blog retreived', async () => {
+ *     await api
+ *         .get('/api/blogs/65a04ef859788ee90e463095')
+ *         .expect(200)
+ *         .expect('Content-Type', /application\/json/)
+ * })
+ */
+});
+
 
 afterAll(async () => {
-	await mongoose.connection.close()
+    await mongoose.connection.close()
 });;
