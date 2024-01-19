a65e2318b2be12776ffc9ae365d2ea1d1f9352f2
FelipeFa6 <felipe.farias.e1@gmail.com>
2024-01-18 22:49:04 -0300

    Finished 4.18

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -10,6 +10,7 @@ const middleware = require('./utils/middleware');
 // routers
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
 // setup connection
 mongoose.set('strictQuery', false);
@@ -29,6 +30,7 @@ app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
